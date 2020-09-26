8d49971c2ffc93314c0e36b4c6e71329d10bf433
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2020-09-26 08:40:51 +0530

    Update sourcecode.py

--- project/sourcecode.py
+++ project/sourcecode.py
@@ -31,7 +31,7 @@ def estimate_coef(x, y):
 #euclidean distance formula
 def eform(x1, y1, x2, y2):
     
-    r = float(sqrt(((x1-x2)**2)+(y1-y1)**2))
+    r = float(sqrt(((x1-x2)**2)+(y1-y2)**2))
     r = round(r,4)
     d.append(r)
     
